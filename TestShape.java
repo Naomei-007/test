34b03a6458526a272655c3f7e31881d632e64ab9
Naomei-007 <62865477+Naomei-007@users.noreply.github.com>
2020-03-30 12:41:36 +0800

    上传文件

---
+++ TestShape.java
@@ -0,0 +1,13 @@
+package Zuoye;
+
+public class TestShape {
+
+	public static void main(String[] args) {
+		Rect rect = new Rect(2.0, 3.0);
+		rect.getter();
+		
+		
+		Circle circle = new Circle(3.5);
+		circle.getter();
+	}
+}
